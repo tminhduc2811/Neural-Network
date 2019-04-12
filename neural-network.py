3d171ae3caf61c23d12fedaf5021d0f980360217
Duc Ta <tminhduc2811@gmail.com>
2019-04-13 01:38:52 +0700

    Reformat code

--- neural-network.py
+++ neural-network.py
@@ -40,13 +40,13 @@ class NeuralNetwork:
 
         # Back propagation
         y = y.reshape(-1, 1)
-        dA = [-(y / A[-1] - (1 -y)/(1-A[-1]))]
+        dA = [-(y / A[-1] - (1 - y) / (1 - A[-1]))]
         dW = []
         db = []
         for i in reversed(range(0, len(self.layers) - 1)):
-            dw_ = np.dot((A[i]).T, dA[-1]*sigmoid_derivative(A[i+1]))
-            db_ = (np.sum(dA[-1]*sigmoid_derivative(A[i+1]), 0)).reshape(-1, 1)
-            dA_ = np.dot(dA[-1]*sigmoid_derivative(A[i+1]), self.W[i].T)
+            dw_ = np.dot((A[i]).T, dA[-1] * sigmoid_derivative(A[i + 1]))
+            db_ = (np.sum(dA[-1] * sigmoid_derivative(A[i + 1]), 0)).reshape(-1, 1)
+            dA_ = np.dot(dA[-1] * sigmoid_derivative(A[i + 1]), self.W[i].T)
             dW.append(dw_)
             db.append(db_)
             dA.append(dA_)
@@ -60,4 +60,4 @@ class NeuralNetwork:
             self.W[i] = self.W[i] - self.alpha * dW[i]
             self.b[i] = self.b[i] - self.alpha * db[i]
 
-        # TODO: Add fit, predict, and calculate_loss functions 
+        # TODO: Add fit, predict, and calculate_loss functions
