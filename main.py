83f003536b5381a5cff6e87aba13eb773522c95c
Duc Ta <tminhduc2811@gmail.com>
2019-04-14 10:48:40 +0700

    Fix dataset

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ data = pd.read_csv('dataset.csv').values
 N, d = data.shape
 X = data[:, 0:d - 1].reshape(-1, d - 1)
 y = data[:, 2].reshape(-1, 1)
-p = NeuralNetwork([X.shape[1], 2, 1], 0.1)
+p = NeuralNetwork([X.shape[1], 5, 1], 0.1)
 p.fit(X, y, 10000, 100)
 # This will print the result = 0
 print('Result for x = [5 0.1] ', p.predict([5, 0.1]))
